d3d6dcfd1d238a74c3e382e51bd08ba7d8de3d80
Gustavo Felipe <gustavofuracao06@hotmail.com>
2017-11-24 12:25:10 -0200

    teste

---
+++ Modelo/PN/pnEventos.cs
@@ -0,0 +1,48 @@
+﻿using Modelo.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo.PN
+{
+    class pnEventos
+    {
+        public static bool Registrar(usuario u)
+        {
+            try {
+                EventosEntities db = new EventosEntities();
+                usuario user = new usuario();
+                //para mexer em um registro fazer a leitura de campo
+                user = db.usuarios.Find(u.CPF);// CONFERIR
+
+
+                user.login = u.login;
+                user.pass = u.pass;
+                user.Nome = u.login;
+                user.Endereco = u.Endereco;
+                user.CEP = u.CEP;
+                user.Nascimento = u.Nascimento;
+                user.cadastro = u.cadastro;
+                user.CPF = u.CPF;
+                user.RG = u.RG;
+                user.r_phone = u.r_phone;
+                user.cel_phone = u.cel_phone;
+                user.newsletter = u.newsletter;
+
+                db.SaveChanges;
+
+
+                return true;
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            }
+
+    }
+}
