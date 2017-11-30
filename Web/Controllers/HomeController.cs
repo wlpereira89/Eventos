921bed4fab0bc9196f27978fdcddb1d803b95511
Gustavo Felipe <gustavofuracao06@hotmail.com>
2017-11-30 21:06:43 -0200

    resolução do problema com controler

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Modelo.DAO;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,9 +11,10 @@ namespace Web.Controllers
 {
     public class HomeController : Controller
     {
+        private EventosEntities db = new EventosEntities();
         public ActionResult Index()
         {
-            return View();
+            return View(db.usuarios.ToList());
         }
 
         public ActionResult About()
@@ -25,6 +29,110 @@ namespace Web.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        // GET: UsuariosC/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            usuario usuario = db.usuarios.Find(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuario);
+        }
+
+        // GET: UsuariosC/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UsuariosC/Create
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "login,pass,Nome,Endereco,CEP,Nascimento,cadastro,CPF,RG,email,r_phone,cel_phone,newsletter")] usuario usuario)
+        {
+            //if (ModelState.IsValid)
+            //{
+            db.usuarios.Add(usuario);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+            //}
+
+            return View(usuario);
+        }
+
+        // GET: UsuariosC/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            usuario usuario = db.usuarios.Find(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuario);
+        }
+
+        // POST: UsuariosC/Edit/5
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "login,pass,Nome,Endereco,CEP,Nascimento,cadastro,CPF,RG,email,r_phone,cel_phone,newsletter")] usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(usuario).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(usuario);
+        }
+
+        // GET: UsuariosC/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            usuario usuario = db.usuarios.Find(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuario);
+        }
+
+        // POST: UsuariosC/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            usuario usuario = db.usuarios.Find(id);
+            db.usuarios.Remove(usuario);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
---
+++ dbo.comentario.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[comentario] (
+    [Id]         INT            NOT NULL,
+    [login]      NVARCHAR (30)  NOT NULL,
+    [id_evento]  INT            NOT NULL,
+    [comentario] NVARCHAR (MAX) NOT NULL,
+    [likes]      SMALLINT       NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_comentario_ToTable] FOREIGN KEY ([login]) REFERENCES [dbo].[usuario] ([login]),
+    CONSTRAINT [FK_comentario _ToTable_1] FOREIGN KEY ([id_evento]) REFERENCES [dbo].[evento] ([Id])
+);
+
