73b90f7c7f9ebda1e356a728a8cd38a4ee784905
Gustavo Felipe <gustavofuracao06@hotmail.com>
2017-12-03 21:28:20 -0200

    Alteração da tabela participante

--- Eventos/dbo/Tables/participante.sql
+++ Eventos/dbo/Tables/participante.sql
@@ -2,6 +2,7 @@
 (
 	[login] NVARCHAR(30) NOT NULL , 
     [id_evento] INT NOT NULL, 
+    [Presenca] BIT NULL, 
     PRIMARY KEY ([id_evento], [login]), 
     CONSTRAINT [FK_participante_ToTable] FOREIGN KEY ([login]) REFERENCES [usuario]([login]), 
     CONSTRAINT [FK_participante_ToTable_1] FOREIGN KEY ([id_evento]) REFERENCES [evento]([Id])
